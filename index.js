c8c64c91520083bd00ae38039ca0468d9c4c07cd
Peter Sampson <petersampson010@gmail.com>
2019-05-11 11:24:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var rank = document.querySelectorAll(`ul.ranked-list li`)
   for (var i=0; i<rank.length; i++) {
-    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
+    rank[i] = parseInt(rank[i]) + n
   }
 }
