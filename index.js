890a893ff1beeb3080fab69ccf4d2d59620d18f4
Peter Sampson <petersampson010@gmail.com>
2019-05-11 11:12:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rank = document.querySelectorAll(ul.ranked-list li)
-  
+  var rank = document.querySelectorAll(`ul.ranked-list li`)
+  for (i=0; i<rank.length; i++) {
+    r
+  }
 }
